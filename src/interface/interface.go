30a1a24ad7afa1643bcbc5284a3d7bbf3ac9345c
Practice Golang <practice.golang@gmail.com>
2018-08-10 15:52:35 +0900

    interface type, type assertion 추가

--- src/interface/interface.go
+++ src/interface/interface.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-/* 구조체, 메서드 */
+/* 구조체 */
 type myString struct {
 	a string
 	b string
@@ -17,6 +17,9 @@ type myNum struct {
 	b int
 }
 
+/* 구조체 */
+
+/* 연결용 메서드 */
 func (s myString) sum() string {
 	return s.a + s.b
 }
@@ -25,9 +28,9 @@ func (n myNum) sum() string {
 	return strconv.Itoa(n.a + n.b)
 }
 
-/* 구조체, 메서드 */
+/* 연결용 메서드 */
 
-/* 인터페이스 #1 */
+/* 인터페이스 메서드 #1 */
 type myObj interface {
 	sum() string
 }
@@ -38,9 +41,9 @@ func printString(o myObj) {
 	fmt.Println(sumed, typed)
 }
 
-/* 인터페이스 #1 */
+/* 인터페이스 메서드 #1 */
 
-/* 인터페이스 #2 */
+/* 인터페이스 메서드 #2 */
 type myObjItf interface {
 	sumItf() interface{}
 }
@@ -72,20 +75,34 @@ func printItf(o myObjItf) {
 	fmt.Println("쓰트링: " + sumedStr)
 }
 
-/* 인터페이스 #2 */
+/* 인터페이스 메서드 #2 */
+
+/* 인터페이스 타입 */
+func mergeString(a interface{}, b string) string {
+	result := a.(string) + b // 그리고, Type Assertion
+	return result
+}
+
+/* 인터페이스 타입 */
 
 func main() {
-	// 인터페이스 #1
+	// 인터페이스 메서드 #1
 	var x myObj = myString{a: "안녕", b: "세상"}
 	var y myObj = myNum{a: 1, b: 2}
 
 	printString(x)
 	printString(y)
 
-	// 인터페이스 #2
+	// 인터페이스 메서드 #2
 	var j myObjItf = myString{a: "헬로", b: "월드"}
 	var k myObjItf = myNum{a: 10, b: 20}
 
 	printItf(j)
 	printItf(k)
+
+	// 인터페이스 타입
+	var itf interface{}
+	itf = "Hello"
+
+	fmt.Println(mergeString(itf, " world"))
 }
